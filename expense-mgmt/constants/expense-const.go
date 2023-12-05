14af55de3a60c2f1ecec4cd763eb566b96334042
Kshitiz Raj <kshtz.pesce@gmail.com>
2023-12-05 12:58:53 +0530

    Added routes and const

---
+++ expense-mgmt/constants/expense-const.go
@@ -0,0 +1,5 @@
+package constants
+
+const (
+	DEFAULT_HTTP_PORT = "8080"
+)
--- expense-mgmt/main.go
+++ expense-mgmt/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"context"
+	"github.com/joho/godotenv"
+	"github.com/raj1kshtz/kharcha-core/conf"
+	"github.com/raj1kshtz/kharcha-kosh/expense-mgmt/constants"
+	"log"
+	"os"
+)
+
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	} else {
+		log.Println("We are getting the env values")
+	}
+
+	conf.LoadConfiguration()
+
+	port := os.Getenv("HTTP_PORT")
+	if len(port) == 0 {
+		port = constants.DEFAULT_HTTP_PORT
+	}
+
+	ctx := context.Background()
 
 }
